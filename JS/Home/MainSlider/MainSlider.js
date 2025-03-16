3e740ca695b480ba4e444e3799bb686a271daa55
unknown <rustam.kerimli.wf@gmail.com>
2025-03-16 18:05:22 +0400

    AZMIU-9 Making Carousel

---
+++ JS/Home/MainSlider/MainSlider.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto", // Eyni vaxtda görünən slayd sayı
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 1.1, // Aktiv slaydın 90%, digərlərinin 5% olması üçün
+      },
+    },
+  });
+});
